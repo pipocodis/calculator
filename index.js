0660e3adb4b88000650c5df2a93724d11b5caad8
elmo <135143680+pipocodis@users.noreply.github.com>
2024-07-25 16:27:09 +0300

    Create function to do given operation on given numbers

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let num1 = prompt("Give first number", 500);
+let num2 = prompt("Give second number", 200);
+let operator = prompt("Choose operator");
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -13,3 +17,17 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
+
+function operate(num1, operator, num2) {
+  return operator === "+"
+    ? add(Number(num1), Number(num2))
+    : operator === "-"
+    ? subtract(num1, num2)
+    : operator === "*"
+    ? multiply(num1, num2)
+    : operator === "/"
+    ? divide(num1, num2)
+    : "fag";
+}
+
+console.log(operate(num1, operator, num2));
