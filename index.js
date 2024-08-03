7571ee8b274efd0a31c281614b0d0628506cd382
elmo <135143680+pipocodis@users.noreply.github.com>
2024-08-03 21:04:37 +0300

    Make the calculator work

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-let num1;
-let num2;
-let operator;
-
 const buttons = document.querySelectorAll("button");
 const display = document.querySelector("#display");
+const operatorButtons = document.querySelectorAll(".operator");
 
 function add(num1, num2) {
   return num1 + num2;
@@ -30,15 +27,46 @@ function operate(num1, operator, num2) {
     ? multiply(num1, num2)
     : operator === "/"
     ? divide(num1, num2)
-    : "fag";
+    : console.log("!!!");
 }
-
-console.log(operate(num1, operator, num2));
 
 buttons.forEach((button) => {
   let displayValue;
+  let num1 = [];
+  let num2 = [];
+  let operator;
+
+  operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (button.textContent === "+") {
+        return (operator = "+");
+      } else if (button.textContent === "-") {
+        return (operator = "-");
+      } else if (button.textContent === "*") {
+        return (operator = "*");
+      } else if (button.textContent === "/") {
+        return (operator = "/");
+      }
+    });
+  });
+
   button.addEventListener("click", () => {
     display.textContent += button.textContent;
     displayValue = display.textContent;
+
+    if (button.textContent === "=") {
+      let displayArray = displayValue
+        .split("")
+        .slice(0, -1)
+        .join("")
+        .split(operator);
+      num1.push(displayArray[0]);
+      num2.push(displayArray[1]);
+      let finalNum1 = num1.toString();
+      let finalNum2 = num2.toString();
+      let result = operate(finalNum1, operator, finalNum2);
+      display.textContent += result;
+      displayValue = display.textContent;
+    }
   });
 });
